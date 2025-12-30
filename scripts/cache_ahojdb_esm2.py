0e53700ca0fbf75f01e664b63952a8c0cf1f527c
Yiyabo <wxxpublic@outlook.com>
2025-12-30 17:52:38 +0800

    重构：将 List 添加到类型导入中

--- scripts/cache_ahojdb_esm2.py
+++ scripts/cache_ahojdb_esm2.py
@@ -10,7 +10,7 @@ import sys
 import platform
 import torch
 from pathlib import Path
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional, Tuple, List
 import warnings
 import argparse
 from tqdm import tqdm
